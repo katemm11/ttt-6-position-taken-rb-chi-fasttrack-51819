b3cc12b72d341201df1763013d55f65098b477ff
Kate Murray <katemeadowsmurray@protonmail.com>
2019-05-18 20:45:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,6 +6,8 @@ def position_taken?(board, index)
     false
   elsif board[index] == ""
     false
+  elsif board[index] == nil
+    false
   elsif board[index] == "X"
     true
   elsif board[index] == "O"
