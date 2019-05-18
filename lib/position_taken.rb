f895a991a58a7b6a6456129bef0f9945cb4deb09
Kate Murray <katemeadowsmurray@protonmail.com>
2019-05-18 20:45:43 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  index.to_i
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X"
+    true
+  elsif board[index] == "O"
+    true
+  end
+  end
