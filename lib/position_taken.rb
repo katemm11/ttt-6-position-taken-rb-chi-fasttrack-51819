ff6d25b4ad722006a7353725ffc67a1ba6b5cc16
Kate Murray <katemeadowsmurray@protonmail.com>
2019-05-18 20:42:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,8 @@
 # code your #position_taken? method here!
-def position_taken?(board, player_input="X")
+def position_taken?(board, player_input)
   if board[player_input] = " "
     false
+  elsif board[player_input] = ""
+    false
+  end
 end
